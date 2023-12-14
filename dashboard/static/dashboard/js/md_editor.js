91723427258e5a3bc9454f51fed30c623834e64f
Irina <irinakhaus@gmail.com>
2023-12-14 23:56:48 +0300

    Add preview for html editor

--- dashboard/static/dashboard/js/md_editor.js
+++ dashboard/static/dashboard/js/md_editor.js
@@ -6,27 +6,47 @@
         let mdTextarea = document.getElementsByClassName("md-editor__textarea")[0];
 
         document.getElementById("bold-button").addEventListener("click", function() {
-            styleText(mdTextarea, "*", "*")
+            styleText(mdTextarea, "<b>", "</b>")
         })
 
         document.getElementById("italic-button").addEventListener("click", function() {
-            styleText(mdTextarea, "_", "_")
+            styleText(mdTextarea, "<i>", "</i>")
         })
 
         document.getElementById("underline-button").addEventListener("click", function() {
-            styleText(mdTextarea, "__", "__")
+            styleText(mdTextarea, "<u>", "</u>")
         })
 
         document.getElementById("spoiler-button").addEventListener("click", function() {
-            styleText(mdTextarea, "~", "~")
+            styleText(mdTextarea, "<s>", "</s>")
         })
 
         document.getElementById("link-button").addEventListener("click", function() {
-            styleText(mdTextarea, "[](", ")")
+            makeElement(mdTextarea, "[]()")
         })
 
         document.getElementById("code-button").addEventListener("click", function() {
             styleText(mdTextarea, "```", "```")
+        })
+
+        let previewButton = document.getElementById('preview-button');
+        let previewField = document.getElementById("preview-field")
+        let previewInfo = document.getElementsByClassName("md-editor__preview_info")[0]
+        let toolBar = document.getElementsByClassName("toolbar-list")[0]
+
+        previewButton.addEventListener("click", function() {
+            previewField.innerHTML = mdTextarea.textContent.replace(/\n/g, '<br>\n');
+            previewField.classList.toggle("show")
+            mdTextarea.classList.toggle("hide")
+            toolBar.classList.toggle("hide")
+            previewInfo.classList.toggle("show")
+            if (previewField.classList.contains("show")) {
+                previewButton.innerHTML = '<span class="material-symbols-outlined">visibility_off</span>'
+            }
+            else {
+                previewButton.innerHTML = '<span class="material-symbols-outlined">visibility</span>'
+            }
+            
         })
     }
 
@@ -42,4 +62,9 @@ function styleText(textatrea, commandStart, commandEnd) {
     
     let selected = textatrea.value.slice(textatrea.selectionStart, textatrea.selectionEnd);
     textatrea.setRangeText(`${commandStart}${selected}${commandEnd}`);
+}
+
+function makeElement(textatrea, element) {
+    textatrea.setRangeText(`${element}`, textatrea.selectionStart, textatrea.selectionEnd, "end");
+    textatrea.focus();
 }
