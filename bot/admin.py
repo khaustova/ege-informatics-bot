881f46ffe356af815789ad0c13d919be6696d7c4
Irina <irinakhaus@gmail.com>
2023-12-18 23:45:25 +0300

    Add categories, tasks and results models

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Quiz
+from .models import User, Category, Subcategory, Assignment, Results
 
 
 @admin.register(User)
@@ -12,4 +12,37 @@ class UsearAdmin(admin.ModelAdmin):
         'created_at',
     )
     
-admin.site.register(Quiz)
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+    )
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'subcategory',
+    )
+
+ 
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'subcategory',
+        'question'[:64],
+        'question_type',
+        'correct_answer'[:64],
+    )
+
+
+@admin.register(Results)
+class ResultsAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'question'[:64],
+        'status',
+    )
+    list_filter = ('user',)
--- bot/models.py
+++ bot/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from mdeditor.fields import MDTextField
+from dashboard.fields import MDEditorField
 
 class User(models.Model):
     user_id = models.PositiveBigIntegerField(
@@ -36,9 +36,129 @@ class User(models.Model):
         verbose_name='Бот заблокирован'
     )
     
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
     def __str__(self):
         return f'@{self.username}' if self.username is not None else f'{self.user_id}'
     
     
-class Quiz(models.Model):
-    question = MDTextField()
+class Category(models.Model):
+    category = models.CharField(
+        max_length=256, 
+        verbose_name='Категория'
+    )
+    
+    @staticmethod
+    def get_default_category():
+        default_category, created = Category.objects.get_or_create(
+            category='Без категории'
+        )
+        return default_category.pk
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.category
+    
+    
+class Subcategory(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        verbose_name='Категория'
+    )
+    subcategory = models.CharField(
+        max_length=256, 
+        verbose_name='Подкатегория'
+    )
+    
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+    
+    def __str__(self):
+        return self.subcategory
+    
+    
+class QuestionTypes(models.TextChoices):
+    SELECT_ONE = 'select_one', 'Выбор ответа'
+    SHORT_REPLY = 'short_reply', 'Краткий ответ'
+    
+    
+class Assignment(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=Category.get_default_category,
+        verbose_name='Категория'
+    )
+    subcategory = models.ForeignKey(
+        'Subcategory',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name='Подкатегория'
+    )
+    question = MDEditorField(verbose_name='Вопрос')
+    question_type = models.CharField(
+        max_length=64,
+        choices=QuestionTypes.choices,
+        verbose_name='Тип вопроса'
+    )
+    correct_answer = models.CharField(
+        max_length=1024, 
+        verbose_name='Правильный ответ'
+    )
+    choice_1 = models.CharField(
+        max_length=1024, 
+        blank=True,
+        null=True,
+        verbose_name='Неправильный ответ 1'
+    )
+    choice_2 = models.CharField(
+        max_length=1024, 
+        blank=True,
+        null=True,
+        verbose_name='Неправильный ответ 2'
+    )
+    choice_3 = models.CharField(
+        max_length=1024, 
+        blank=True,
+        null=True,
+        verbose_name='Неправильный ответ 3'
+    )
+    
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+    
+    def __str__(self):
+        return f'{self.question[:64]}'
+    
+    
+class Results(models.Model):
+    user = models.ForeignKey(
+        'User', 
+        on_delete=models.CASCADE, 
+        verbose_name='Пользователь'
+    )
+    question = models.ForeignKey(
+        'Assignment', 
+        on_delete=models.CASCADE, 
+        verbose_name='Задание'
+    )
+    status = models.BooleanField(verbose_name='Правильный ответ')
+    
+    class Meta:
+        verbose_name = 'Результат'
+        verbose_name_plural = 'Результаты'
+    
+    def __str__(self):
+        status = 'правильно' if self.status else 'неправильно'
+        return f'{self.user}: {self.question.question[:64]} - {status}'
