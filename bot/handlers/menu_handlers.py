dffd2f63684503b0864aa5b0d459bf33a86a8076
Irina <irinakhaus@gmail.com>
2023-12-19 17:37:12 +0300

    Add cancel handlers

--- bot/handlers/menu_handlers.py
+++ bot/handlers/menu_handlers.py
@@ -1,10 +1,13 @@
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import Command, StateFilter
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
 from .exam_handlers import create_exam
 from ..main import dp
 from ..models import User
-from ..db_methods import get_categories, get_subcategories
+from ..states.take_exam import TakeExam
+from ..db_methods import get_categories, get_subcategories, get_questions
 from ..keyboards.factories import CategoryCallbackFactory, SubcategoryCallbackFactory
 from ..keyboards.main_menu_keyboards import (
     make_menu_keyboard, 
@@ -33,16 +36,28 @@ async def start_command(message: Message):
     )
 
 
-@dp.callback_query(CategoryCallbackFactory.filter())
-async def get_category(callback: CallbackQuery, callback_data: CategoryCallbackFactory):
+@router.message(Command(commands='cancel'), ~StateFilter(default_state))
+async def process_cancel_command_state(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text='Вы вышли из машины состояний\n\n'
+    )
+
+
+@dp.callback_query(StateFilter(default_state), CategoryCallbackFactory.filter())
+async def get_category(callback: CallbackQuery, callback_data: CategoryCallbackFactory, state: FSMContext):
     subcategories = await get_subcategories(callback_data.category_id)
     await callback.message.answer(
-        text=callback_data.pack(),
+        text='Теперь выбери подкатегорию.',
         reply_markup=make_subcategory_keyboard(subcategories)
     )
     await callback.answer()
+    await state.set_state(TakeExam.start_exam)
     
     
-@dp.callback_query(SubcategoryCallbackFactory.filter())
-async def get_subcategory(callback: CallbackQuery, callback_data: SubcategoryCallbackFactory):
-    await create_exam(callback.message.chat.id, callback.from_user.id)
+@dp.callback_query(StateFilter(TakeExam.start_exam), SubcategoryCallbackFactory.filter())
+async def get_subcategory(callback: CallbackQuery, callback_data: SubcategoryCallbackFactory, state: FSMContext):
+    questions = await get_questions(callback.from_user.id, callback_data.subcategory_id)
+    await state.set_state(TakeExam.next_question)
+    await create_exam(callback.message.chat.id, callback.from_user.id, questions, state)
+
