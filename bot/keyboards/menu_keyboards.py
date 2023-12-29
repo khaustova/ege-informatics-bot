14dbcbe7d7623adbf23553fd231572a7c9b168e1
Irina <irinakhaus@gmail.com>
2023-12-29 23:43:51 +0300

    Update keyboards

--- bot/keyboards/menu_keyboards.py
+++ bot/keyboards/menu_keyboards.py
@@ -1,8 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from .factories import CategoryCallbackFactory
-from ..db_methods import get_categories
-from ..models import Results, Assignment
+from ..models import Assignment, Category, Results
 from ..services.vocabulary import BotEmoji
 
 
@@ -14,19 +13,18 @@ async def make_menu_keyboard(user_id) -> InlineKeyboardMarkup:
     categories_keyboard_builder = InlineKeyboardBuilder()
     categories_buttons: list[InlineKeyboardButton] = []
     
-    categories = await get_categories() 
-    for category in categories:
-        done = await Results.objects.filter(
+    for category in await Category.category_manager.get_categories():
+        done: int = await Results.objects.filter(
             user__user_id=user_id, 
-            category__category=category['category']
+            category__category=category.category
         ).acount()
-        all = await Assignment.objects.filter(
-            category__category=category['category']
+        all: int = await Assignment.objects.filter(
+            category__category=category.category
         ).acount()
         categories_buttons.append(InlineKeyboardButton(
-            text=f'{category["category"]} ({done}/{all})',
+            text=f'{category.category} ({done}/{all})',
             callback_data=CategoryCallbackFactory(
-                category_id=category['id']
+                category_id=category.id
                 ).pack()
             )
         )
--- bot/keyboards/statistics_keyboards.py
+++ bot/keyboards/statistics_keyboards.py
@@ -1,8 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from .factories import StatisticCategoryCallbackFactory
-from ..db_methods import get_categories
-from ..models import Results, Assignment
+from ..models import Assignment, Category, Results
 from ..services.vocabulary import BotEmoji
 
 
@@ -14,22 +13,21 @@ async def make_statistics_keyboard(user_id: int | str) -> InlineKeyboardMarkup:
     categories_keyboard_builder = InlineKeyboardBuilder()
     categories_buttons: list[InlineKeyboardButton] = []
     
-    categories = await get_categories() 
-    for category in categories:
+    for category in await Category.category_manager.get_categories() :
         done = await Results.objects.filter(
             user__user_id=user_id, 
-            category__category=category['category']
+            category__category=category.category
         ).acount()
         success = await Results.objects.filter(
             user__user_id=user_id, 
-            category__category=category['category'],
+            category__category=category.category,
             status=True
         ).acount()
         all = await Assignment.objects.filter(
-            category__category=category['category']
+            category__category=category.category
         ).acount()
         categories_buttons.append(InlineKeyboardButton(
-            text=f'{category["category"]}',
+            text=f'{category.category}',
             callback_data='pass'
             )
         )
@@ -41,7 +39,7 @@ async def make_statistics_keyboard(user_id: int | str) -> InlineKeyboardMarkup:
         categories_buttons.append(InlineKeyboardButton(
             text=f'{BotEmoji.red_cross} Сбросить',
             callback_data=StatisticCategoryCallbackFactory(
-                category_id=category['id']
+                category_id=category.id
                 ).pack()
             )
         )
