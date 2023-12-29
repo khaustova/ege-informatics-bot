dc2e86665b5c446ecc552c427a01f414e1382541
Irina <irinakhaus@gmail.com>
2023-12-29 23:43:15 +0300

    Update states

--- bot/states/take_exam.py
+++ bot/states/take_exam.py
@@ -2,7 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class TakeExam(StatesGroup):
-    start_exam = State()
     get_question = State()
     get_result = State()
     next_question = State()
